a55ff7b6c304231efac8f3d1e7f985610c1c9017
stevenzim <steven_e_zimmerman@yahoo.com>
2015-01-26 08:30:00 +0000

    Added Examples

--- projects/climReport/Examples.py
+++ projects/climReport/Examples.py
@@ -9,10 +9,13 @@ for station in stationFile:
 	stationDetails = station.split(',')
 	report = NWSclimReport.ClimateReport()
 	reportLines = report.getReport("sew",stationDetails[1])
-	skyCover = report.getSkyCover()
-	skyCover = report.getWinds()
-	json.dump(stationDetails[4], open("t.txt",'a'))
-	json.dump(report.avg_sky_cvg, open("t.txt",'a'))
-	json.dump(report.winds, open("t.txt",'a'))	
+	report.getSkyCover()
+	report.getWinds()
+	reportDict = {}
+	masterDict = {}
+	reportDict['SKY'] = report.avg_sky_cvg
+	reportDict['WINDS'] = report.winds
+	masterDict[stationDetails[4]] = reportDict
+	json.dump(masterDict, open("t.txt",'a'), sort_keys=True, indent=4, separators=(',', ': '))
 	print stationDetails[4]
 	
