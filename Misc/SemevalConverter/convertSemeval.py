88e974ea1ce87125e05e8ab65c9bc6e0e0d2997d
Steven Zimmerman <stevenedwardzimmerman@gmail.com>
2015-05-27 12:30:57 +0100

    A working version of Semeval csv to json converter

--- Misc/SemevalConverter/convertSemeval.py
+++ Misc/SemevalConverter/convertSemeval.py
@@ -9,32 +9,41 @@
 #TODO:  Consider feature reduction.  e.g. change @username to user and all http links --> URL
 #TODO:	Consider is it better to leave classes as neg,neut,pos or convert to -1,0,1
 
+# usage 'python .\convertSemeval.py TestFile.csv semeval-train.json'
+
 import csv
 import json
 import sys
 
-print str(sys.argv)
-
-args = str(sys.argv)
+args = sys.argv
 inputCSV = args[1]
 outputJSON = args[2]
 
-# usage 'python .\convertSemeval.py TestFile.csv semeval-train.json'
 
-print inputCSV
-print outputJSON
+def convertSentToNum(sentimentString):
+    '''
+    Converts sentiment string to a numeric value i.e. positive = 1
+    '''
+    if sentimentString == 'positive':
+        return 1
+    if sentimentString == 'neutral':
+        return 0
+    if sentimentString == 'negative':
+        return -1
+
 def buildOutputDict(csvRow):
     '''
     Given csv row
     converts to desired dictionary format for eventual output to JSON
     '''
     outputDict = {'tweet_id' : csvRow['tweet_id'],
-				'semeval_id' : csvRow['semeval_id'],
+                'semeval_id' : csvRow['semeval_id'],
                 'text_orig' : csvRow['tweet'],  
                 #'text_converted' : links_convert(csvRow['tweet']),  
                 #'loc_state' : csvRow['state'],
                 #'loc_city' : csvRow['location'],
                 'sentiment_orig' : csvRow['sentiment'],
+                'sentiment_num' : convertSentToNum(csvRow['sentiment']),
                 #'sentiment_converted' : sentiment_convert(sentiList[1:4]), 
                 #'topic_wx_orig' : sentiList[-1],
                 #'topic_wx_converted' : topic_convert(sentiList[-1]),  
@@ -46,23 +55,24 @@ def buildOutputDict(csvRow):
 #MAIN PART OF PROGRAM. 
 #Takes in semeval csv file and converts one row at a time to desired dictionary format.  
 #NOTE: You must convert semeval file from tab delimited to comma delimited and add correct headers
-#TODO: Could change the code from here below to be a function that takes in csv and json filenames
 
 myList = []
 currentRow = 1
 with open(inputCSV) as csvfile:
-#with open('TestFile.csv') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
         print currentRow
         currentRow += 1
-
-        myList.append(buildOutputDict(row))
+        if row['tweet'] == 'Not Available':
+            continue
+        else:
+            myList.append(buildOutputDict(row))
 
 
 
 #Pretty print to JSON
 #TODO: Reorganize code, so that I can import the helper function
 #with open('semeval-train.json', 'w') as outfile:
+#with open('Test.json', 'w') as outfile:
 with open(outputJSON, 'w') as outfile:
     json.dump(myList, outfile ,sort_keys=True, indent=4, separators=(',', ': '))
