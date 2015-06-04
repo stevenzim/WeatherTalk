debcc07d42bdf1e5846396d320f731cc79aa7592
stevenzim <stevenedwardzimmerman@gmail.com>
2015-06-04 16:29:25 +0100

    built initial transformer tests and first draft of transformers

--- ModelBuilder/tests/transformers_tests.py
+++ ModelBuilder/tests/transformers_tests.py
@@ -1,16 +1,30 @@
 from nose.tools import *
-import twxeety.vectorizer as vectorizer
-import numpy
-from scipy.sparse import csr_matrix
+import twxeety.transformers as transformer
+import twxeety.helper as helper
+
+listOfDicts = [{'doc': 'abc', 'triple': [0,5,6]}, {'doc': 'def', 'triple': [1,2,3]}]
+#listOfTriples = [['text','pos','conf']]
+listOfTriples1 = [[['?',',',.9]],[['word','N',.9]]]
+listOfTriples2 = [[['hello','#',.9],['http://you.com','U',.9]],[['word','N',.9]]]
 
 
-#added twitter features
-def test_merge_csr_numpy():
-    '''Test for merger of csr array with numpy array'''
-    #TODO: Add test for error when dimensions don't match
-    a = csr_matrix(numpy.zeros((2,2)))
-    b = numpy.zeros((2,2))
-    c = vectorizer.mergeCSRwithNumpyArrays(a,b)
-    d = numpy.zeros((2,4))
-    assert_equal(c.toarray().all(),d.all())
+def test_docs_and_triples_extractor():
+	'''test to confirm docs and triples are correctly extracted from list of dicts'''
+	d = transformer.DocsAndTriplesExtractor()
+	docs,triples = d.transform(listOfDicts,'doc','triple')
+	assert_equal(docs,['abc','def'])
+	assert_equal(triples,[[0,5,6],[1,2,3]])
 
+def test_text_features_extractor():
+	'''test to confirm features are correctly extracted from triples'''
+	d = transformer.TextFeaturesExtractor()
+	#test 1 - confirm question mark is True
+	featureDict = d.transform(listOfTriples1)
+	assert_equal(featureDict["questmark_present"],[True,False])
+    assert_equal(featureDict["urloremail_present"],[False,False])
+	assert_equal(featureDict["hashtag_present"],[False,False])
+	#test 2 - confirm url,email and hashtag test true
+	featureDict = d.transform(listOfTriples2)
+	assert_equal(featureDict["questmark_present"],[False,False])
+	assert_equal(featureDict["urloremail_present"],[True,False])
+	assert_equal(featureDict["hashtag_present"],[True,False])
--- ModelBuilder/twxeety/transformers.py
+++ ModelBuilder/twxeety/transformers.py
@@ -6,120 +6,112 @@ from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.feature_extraction.text import (CountVectorizer, TfidfTransformer, TfidfVectorizer)
 from sklearn.svm import SVC
+import helper
 
-class TextStats(BaseEstimator, TransformerMixin):
-    """Extract features from each document for DictVectorizer"""
+
+class DocsAndTriplesExtractor(BaseEstimator, TransformerMixin):
+    '''Provided a list of dictionaries containing triples created by Twitter NLP
+    & normalised strings --> return a document list of string and triples list 
+    document list can be passed to tfidfVectorizer for example
+    and triples list can be passed to other transformer'''
     def fit(self, x, y=None):
         return self
-    def transform(self, listOftriples):
-        return [{'length': len(tripleSetCurrentDoc)} for tripleSetCurrentDoc in listOftriples]
+    def transform(self, listOfDicts ,\
+        docsKeyName ="normal_string_rtrgo" , triplesKeyName = "tagged_tweet_triples"):
+        docsList = []
+        triplesList = []
+        for dict in listOfDicts:
+            docsList.append(dict[docsKeyName])
+            triplesList.append(dict[triplesKeyName])
+        return docsList, triplesList
 
-
-class TextStats(BaseEstimator, TransformerMixin):
-    """Extract features from each document for DictVectorizer"""
+class TextFeaturesExtractor(BaseEstimator, TransformerMixin):
+    """Extract features from each document which can then be passed to DictVectorizer
+    NOTE: you can pass in list of keys to drop, where key name is representitive of features
+    you don't want"""
+    #TODO: Verify that when no features returned, that it will still append to other features
     def fit(self, x, y=None):
         return self
-    def transform(self, listOfTriples):
-        return [{'length': len(tripleSetCurrentDoc),\
-        'test': len(tripleSetCurrentDoc)} for tripleSetCurrentDoc in listOfTriples]
-        #'num_sentences': text.count('.')}
+    def transform(self, listOfTriples, keysToDrop=[]):
+        listOfStats = [{'length': len(tripleSetCurrentDoc),\
+                    'test': len(tripleSetCurrentDoc)}\
+                     for tripleSetCurrentDoc in listOfTriples]
+        listToReturn = dropKeysVals(listOfStats, keysToDrop)
+        return listToReturn
+
 
           
-def dropKeysVals(listOfDicts, keysToDrop = []):
-    '''Remove specified Keys and Values from dictionaries stored in list based 
-    on provided list of keysToDrop'''
-    #TODO: Perhaps there is a map reduce fucntion that could do this better???
-    #       similar to map over in clojure
-    #TODO: Perhaps it is less confusing to base it on keys to keep (i.e. features to keep)
-    #       vs. keys to drop (i.e. features to drop)
-    #TODO: Determine how to pass in list of args to transformers, 
-    #       which will then be passed to this function
-    if type(keysToDrop) != type([]):
-        raise ValueError('keysToDrop must be in a list!')
-    #no keys to drop so return full list of dicts
-    if len(keysToDrop) == 0:
-        return listOfDicts
-    #iterate over entire list of dicts --> for each dict iterate over list of keys
-    # to drop and drop key --> then add updated dictionary to return list 
-    returnList = []
-    for dictionary in listOfDicts:
-        print dictionary
-        for keyName in keysToDrop:
-            try:
-                dictionary.pop(keyName)
-            except:
-                raise ValueError('invalid key name in list!')
-                continue
-        returnList.append(dictionary)
-    return returnList             
+#           
 
-lod = [{'length': len(tripleSetCurrentDoc),'test': len(tripleSetCurrentDoc)}\
- for tripleSetCurrentDoc in listOfTriples]
+#lod = [{'length': len(tripleSetCurrentDoc),'test': len(tripleSetCurrentDoc)}\
+# for tripleSetCurrentDoc in listOfTriples]
 
-               
-pipeline = Pipeline([
-        #('selector', ItemSelector(key='body')),
-        ('stats', TextStats()),  # returns a list of dicts
-        ('vect', DictVectorizer()),  # list of dicts -> feature matrix
-    ])
-
-import helper
-listOfDicts = helper.loadJSONfromFile('data.json')
-def getListOfTriples(listOfDicts):
-    docsList = []
-    triplesList = []
-    for dict in listOfDicts:
-        docsList.append(dict["normal_string_rtrgo"])
-        triplesList.append(dict["tagged_tweet_triples"])
-    return docsList,triplesList
+#               
+#pipeline = Pipeline([
+#        #('selector', ItemSelector(key='body')),
+#        ('stats', TextStats()),  # returns a list of dicts
+#        ('vect', DictVectorizer()),  # list of dicts -> feature matrix
+#    ])
 
 
-listOfDocs,listOfTriples = getListOfTriples(listOfDicts)
-        
-featureVector = pipeline.fit_transform(listOfTriples)
+#listOfDicts = helper.loadJSONfromFile('data.json')
 
-featureVector.toarray()
+
+#def getListOfTriples(listOfDicts):
+#    docsList = []
+#    triplesList = []
+#    for dict in listOfDicts:
+#        docsList.append(dict["normal_string_rtrgo"])
+#        triplesList.append(dict["tagged_tweet_triples"])
+#    return docsList,triplesList
+
+
+#listOfDocs,listOfTriples = getListOfTriples(listOfDicts)
+#        
+#featureVector = pipeline.fit_transform(listOfTriples)
+
+#featureVector.toarray()
 
 
 
-class NormalisedDocsList(BaseEstimator, TransformerMixin):
-    """Extract normalized string from each document, put into list of docs
-     to pass to desired text vectorizer e.g. CountVectorizer or TfidfVectorizer"""
-    def fit(self, x, y=None):
-        return self
-    def transform(self, listOftriples):
-        docsList = []
-        for tripleSetCurrentDoc in listOftriples:
-            tokenList = []
-            for triple in tripleSetCurrentDoc:
-                token = triple[0]
-                #normalising steps some adapted from RTRGO paper
-                token = token.lower()
-                token = token.replace("#","")
-                tokenList.append(token)
-            docsList.append(' '.join(tokenList))
-        return docsList
-            
+#class NormalisedDocsList(BaseEstimator, TransformerMixin):
+#    """Extract normalized string from each document, put into list of docs
+#     to pass to desired text vectorizer e.g. CountVectorizer or TfidfVectorizer"""
+#    def fit(self, x, y=None):
+#        return self
+#    def transform(self, listOftriples):
+#        docsList = []
+#        for tripleSetCurrentDoc in listOftriples:
+#            tokenList = []
+#            for triple in tripleSetCurrentDoc:
+#                token = triple[0]
+#                #normalising steps some adapted from RTRGO paper
+#                token = token.lower()
+#                token = token.replace("#","")
+#                tokenList.append(token)
+#            docsList.append(' '.join(tokenList))
+#        return docsList
+#            
 
-n = NormalisedDocsList()
-n.transform(listOfTriples)
+#n = NormalisedDocsList()
+#n.transform(listOfTriples)
 
 
 
 
-countvecpipe = Pipeline([
-        #('selector', ItemSelector(key='body')),
-        ('normed-docs', NormalisedDocsList()),  # returns a list of normalised docs e.g. text from doc
-        ('count-vect', CountVectorizer()),  # vectorized n-gram counts
-    ])
+#countvecpipe = Pipeline([
+#        #('selector', ItemSelector(key='body')),
+#        ('normed-docs', NormalisedDocsList()),  # returns a list of normalised docs e.g. text from doc
+#        ('count-vect', CountVectorizer()),  # vectorized n-gram counts
+#    ])
 
-        
-countVector = countvecpipe.fit_transform(listOfTriples)
+#        
+#countVector = countvecpipe.fit_transform(listOfTriples)
 
-countVector.toarray()
+#countVector.toarray()
 
-#merge features
-featpipe = FeatureUnion([('n-gram-vec',countvecpipe),('feat-vec',pipeline)])
+##merge features
+#featpipe = FeatureUnion([('n-gram-vec',countvecpipe),('feat-vec',pipeline)])
 
 
 
@@ -128,32 +120,32 @@ featpipe = FeatureUnion([('n-gram-vec',countvecpipe),('feat-vec',pipeline)])
 
 
 
-def mergeCSRwithNumpyArrays(csr_array,numpy_array):
-    '''
-    Inputs 1- csr_matrix (usually a sparse n-gram matrix)
-          2- numpy_array(usually non n-gram features)
-    Outputs a sparse csr_matrix to be passed into classifiers
-    NOTE: must be matching dimensions and must have same dimension
-    on concatenating axis'''
-    #TODO: If time, throw in error handling
-    concatedArray = numpy.concatenate((csr_array.toarray(),numpy_array), axis = 1)
-    return csr_matrix(concatedArray)
+#def mergeCSRwithNumpyArrays(csr_array,numpy_array):
+#    '''
+#    Inputs 1- csr_matrix (usually a sparse n-gram matrix)
+#          2- numpy_array(usually non n-gram features)
+#    Outputs a sparse csr_matrix to be passed into classifiers
+#    NOTE: must be matching dimensions and must have same dimension
+#    on concatenating axis'''
+#    #TODO: If time, throw in error handling
+#    concatedArray = numpy.concatenate((csr_array.toarray(),numpy_array), axis = 1)
+#    return csr_matrix(concatedArray)
 
 
-class Vectorize(object):
-    def __init__(self,ngram_range=(1, 1),vocabulary=None,norm='l2'):
-        self.countVec =  CountVectorizer(analyzer=string.split, min_df=1,\
-                            ngram_range = ngram_range,vocabulary=vocabulary)
-        self.tfidfVec =  TfidfVectorizer(analyzer=string.split, min_df=1,\
-                            ngram_range = ngram_range,vocabulary=vocabulary,norm=norm)
-#countVec =  CountVectorizer(analyzer=string.split, min_df=1,ngram_range=(1, 1),vocabulary=None)        
-        self.ngramArray = None
-        self.vocabulary = None
-     
-    def countTransform(self,corpus):
-        self.ngramArray = self.countVec.fit_transform(corpus)
-        self.vocabulary = self.countVec.vocabulary_
-    
-    def tfidfTransform(self,corpus):
-        self.ngramArray = self.countVec.fit_transform(corpus)
-        self.vocabulary = self.countVec.vocabulary_
+#class Vectorize(object):
+#    def __init__(self,ngram_range=(1, 1),vocabulary=None,norm='l2'):
+#        self.countVec =  CountVectorizer(analyzer=string.split, min_df=1,\
+#                            ngram_range = ngram_range,vocabulary=vocabulary)
+#        self.tfidfVec =  TfidfVectorizer(analyzer=string.split, min_df=1,\
+#                            ngram_range = ngram_range,vocabulary=vocabulary,norm=norm)
+##countVec =  CountVectorizer(analyzer=string.split, min_df=1,ngram_range=(1, 1),vocabulary=None)        
+#        self.ngramArray = None
+#        self.vocabulary = None
+#     
+#    def countTransform(self,corpus):
+#        self.ngramArray = self.countVec.fit_transform(corpus)
+#        self.vocabulary = self.countVec.vocabulary_
+#    
+#    def tfidfTransform(self,corpus):
+#        self.ngramArray = self.countVec.fit_transform(corpus)
+#        self.vocabulary = self.countVec.vocabulary_
