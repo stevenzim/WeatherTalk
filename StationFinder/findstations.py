cac2616d735c681d4bdb4f33700a805d9de57f1e
stevenzim <stevenedwardzimmerman@gmail.com>
2015-06-09 17:24:42 +0100

    Changed function to a class

--- StationFinder/findstations.py
+++ StationFinder/findstations.py
@@ -1,36 +1,46 @@
 import psycopg2
 import sys
 
-def getStationList(twitterCoords,maxStations = 3):
-    '''
-    Pass in a list of coordinates in [longitude,latitude] format
-    Returns a list of tuples containing top 3 
-    '''
-    connection = psycopg2.connect("dbname=weather user=postgres password=postgres") 
-    cursor = connection.cursor()
 
-    stationCoordString = str(tuple(twitterCoords))
+#TODO: Create tests
 
-    sql = "SELECT ICAO_ID, name, latitude, longitude, location,\
-           round((location <@> point" + stationCoordString + ")::numeric, 3) as miles \
-    FROM stations \
-    ORDER BY location <-> point" + stationCoordString + " \
-    limit " + str(maxStations) + ";"
+class Stations(object):
+    '''
+    usage:
+    import findstations
+    db = findstations.Stations()
+    db.getStationList([-75.,44.])
     
-    listToReturn = []
-    cursor.execute(sql)
-    listOfStations = cursor.fetchall()
-    for record in listOfStations:
-        stationID = record[0]
-        stationDistance = record[-1].__float__()
-        listToReturn.append((stationID,stationDistance))
-    
-    
-    connection.close()
-    
-    return listToReturn
+    returns [('KSLK', 48.312), ('KMSS', 64.681), ('KART', 51.219)]
+    '''
+    def __init__(self):
+        self.connection = psycopg2.connect("dbname=weather user=postgres password=postgres") 
+        self.cursor = self.connection.cursor()
+
+    def getStationList(self,twitterCoords,maxStations = 3):
+        '''
+        Pass in a list of coordinates in [longitude,latitude] format
+        Returns a list of tuples containing top 3 
+        '''
+        stationCoordString = str(tuple(twitterCoords))
+
+        #sql statement to retrieve sorted list of stations with distances in statue miles
+        sql = "SELECT ICAO_ID, name, latitude, longitude, location,\
+               round((location <@> point" + stationCoordString + ")::numeric, 3) as miles \
+        FROM stations \
+        ORDER BY round((location <@> point" + stationCoordString + ")::numeric, 3) \
+        limit " + str(maxStations) + ";" 
         
-
+        
+        listToReturn = []
+        self.cursor.execute(sql)
+        listOfStations = self.cursor.fetchall()
+        for record in listOfStations:
+            stationID = record[0]
+            stationDistance = record[-1].__float__()
+            listToReturn.append((stationID,stationDistance))
+        
+        return listToReturn    
 
 
 
