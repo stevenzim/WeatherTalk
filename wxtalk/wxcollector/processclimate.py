7c4d4c94d04ad3c285bf68e01a132c0cc5e9dc40
stevenzim <stevenedwardzimmerman@gmail.com>
2015-06-29 19:22:07 +0100

    Working code to clean up climate dailies and put in desired formate (without averages)

--- wxtalk/wxcollector/processclimate.py
+++ wxtalk/wxcollector/processclimate.py
@@ -44,6 +44,8 @@ import os
 import re
 import datetime
 
+from wxtalk.db import dbfuncs as db
+
 
 def setSunToMins(sunriseString,sunsetString):
     '''Given sunrise/sunset in 759 AM and 804PM format, returns total potential sunshine in minutes'''
@@ -70,6 +72,7 @@ def setSunToMins(sunriseString,sunsetString):
     pmTotal = pmHours*60 + pmMins
     return amTotal + pmTotal
 
+
 def setWindsToKnots(windsInMPH):
     '''Convert wind mph to wind knots'''
     if type(windsInMPH) != type(-999.9):
@@ -78,6 +81,7 @@ def setWindsToKnots(windsInMPH):
         return -999.9
     else:
         return windsInMPH/1.15
+
 
 def setTempsToCelsius(tempsInF):
     '''Convert temp in F to C'''
@@ -98,12 +102,13 @@ def setDateToDatetime(icaoCode,dateString):
     try:
         startTime = ''
         endTime = ''
-        if icaoCode =='KJFK':
-            startTime ='05:00:00'
-            endTime = '04:59:59'
-        if icaoCode =='KSFO':
-            startTime ='08:00:00'
-            endTime = '07:59:59'
+        s = db.Connector()
+        sqlstring = 'SELECT daily_report_start_time_utc, daily_report_end_time_utc FROM weather.stations\
+                    WHERE icao_id = \'' + icaoCode + '\''
+        s.cursor.execute(sqlstring)
+        stationDetails = s.cursor.fetchone()
+        startTime =stationDetails[0]
+        endTime = stationDetails[1]   
         date = datetime.datetime.strptime(dateString,"%Y-%m-%d")
         dayAhead = date + datetime.timedelta(days = 1)
         endDayString = dayAhead.strftime("%Y-%m-%d")
@@ -111,9 +116,8 @@ def setDateToDatetime(icaoCode,dateString):
         endDateTime = endDayString + " " + endTime
         print [startDateTime,endDateTime]
         return [startDateTime,endDateTime]
-    except:
+    except Exception as error:
         raise ValueError("ICAO code not in station list, or date string not in correct format")
-
 
 
 def setFloat(dict,key):
@@ -122,6 +126,7 @@ def setFloat(dict,key):
         return float(dict[key])
     except:
         return -999.9
+
 
 def setBool(dict,key):
     '''Set to integer 1 for true, 0 for false 999 for doesn't exist or missing'''
@@ -132,7 +137,7 @@ def setBool(dict,key):
             return 0
     except:
         return -999
-
+        
 
 #These are keys that may or may not occur in each report, but they must be dropped
 keysToDrop =         ["<type_'float'>",\
@@ -170,108 +175,108 @@ def setClimateDict(climateFlatDict):
     #First get rid of any non-needed keys
     for key in keysToDrop:
         climateFlatDict.pop(key,None)
+        
+    datesTimes = setDateToDatetime(climateFlatDict["icao_id"],climateFlatDict["report-date"])
     
-    
-"precipitation_liquid_departure" = setFloat(climateFlatDict,"precipitation_liquid_departure")
-"precipitation_liquid_new_record" = setBool(climateFlatDict,"precipitation_liquid_new_record")
-"precipitation_liquid_observed" = setFloat(climateFlatDict,"precipitation_liquid_observed")
-"precipitation_snowfall_departure" = setFloat(climateFlatDict,"precipitation_snowfall_departure")
-"precipitation_snowfall_new_record" = setBool(climateFlatDict,"precipitation_snowfall_new_record")
-"precipitation_snowfall_observed" = setFloat(climateFlatDict,"precipitation_snowfall_observed")
-"report-date" = climateFlatDict["report-date"]
-"skies_average_sky_cover" = setFloat(climateFlatDict,"skies_average_sky_cover")
-"icao_id" = climateFlatDict["station"]
-"sun_sunrise" = climateFlatDict["sun_sunrise"]
-"sun_sunset" = climateFlatDict["sun_sunset"]
-"temperature_maximum_departure" = setFloat(climateFlatDict,"temperature_maximum_departure")
-"temperature_maximum_new_record" = setBool(climateFlatDict,"temperature_maximum_new_record")
-"temperature_maximum_observed" = setFloat(climateFlatDict,"temperature_maximum_observed")
-"temperature_minimum_departure" = setFloat(climateFlatDict,"temperature_minimum_departure")
-"temperature_maximum_new_record" = setBool(climateFlatDict,"temperature_minimum_new_record")
-"temperature_minimum_observed" = setFloat(climateFlatDict,"temperature_minimum_observed")
-"winds_average_wind_speed" = setFloat(climateFlatDict,"winds_average_wind_speed")
-"winds_highest_gust_speed" = setFloat(climateFlatDict,"winds_highest_gust_speed")
-"winds_highest_wind_speed" = setFloat(climateFlatDict,"winds_highest_wind_speed")
-
-"two_day_avg_precipitation_liquid_departure" = -999.9
-"two_day_avg_precipitation_liquid_observed" = -999.9
-"two_day_avg_precipitation_snowfall_departure" = -999.9
-"two_day_avg_precipitation_snowfall_observed" = -999.9
-"two_day_avg_temperature_maximum_departure" = -999.9
-"two_day_avg_temperature_maximum_observed" = -999.9
-"two_day_avg_temperature_minimum_departure" = -999.9
-"two_day_avg_temperature_minimum_observed" = -999.9
-"two_day_avg_winds_average_wind_speed" = -999.9
-
-"three_day_avg_precipitation_liquid_departure" = -999.9
-"three_day_avg_precipitation_liquid_observed" = -999.9
-"three_day_avg_precipitation_snowfall_departure" = -999.9
-"three_day_avg_precipitation_snowfall_observed" = -999.9
-"three_day_avg_temperature_maximum_departure" = -999.9
-"three_day_avg_temperature_maximum_observed" = -999.9
-"three_day_avg_temperature_minimum_departure" = -999.9
-"three_day_avg_temperature_minimum_observed" = -999.9
-"three_day_avg_winds_average_wind_speed" = -999.9
-
-"seven_day_avg_precipitation_liquid_departure" = -999.9
-"seven_day_avg_precipitation_liquid_observed" = -999.9
-"seven_day_avg_precipitation_snowfall_departure" = -999.9
-"seven_day_avg_precipitation_snowfall_observed" = -999.9
-"seven_day_avg_temperature_maximum_departure" = -999.9
-"seven_day_avg_temperature_maximum_observed" = -999.9
-"seven_day_avg_temperature_minimum_departure" = -999.9
-"seven_day_avg_temperature_minimum_observed" = -999.9
-"seven_day_avg_winds_average_wind_speed" = -999.9
-
-"thirty_day_avg_precipitation_liquid_departure" = -999.9
-"thirty_day_avg_precipitation_liquid_observed" = -999.9
-"thirty_day_avg_precipitation_snowfall_departure" = -999.9
-"thirty_day_avg_precipitation_snowfall_observed" = -999.9
-"thirty_day_avg_temperature_maximum_departure" = -999.9
-"thirty_day_avg_temperature_maximum_observed" = -999.9
-"thirty_day_avg_temperature_minimum_departure" = -999.9
-"thirty_day_avg_temperature_minimum_observed" = -999.9
-"thirty_day_avg_winds_average_wind_speed" = -999.9
-
-    return{
-        #info about report
-        'icao_id' : metarList[0],                # ICAO Code e.g. KSEA
-        'observation_time' : metarList[1][:-1] + '+00:00',     # Date and Time in UTC  --> Drop Z at end --> add timezone offset e.g. 2015-05-22T14:02:00
-        #temps
-        'temp_c' : float(metarList[2]),             # Temp C  38.6
-        'dewpoint_c' : float(metarList[3]),         # Dewpoint Temp C 24.5
-        #winds
-        'wind_dir_degrees' : int(metarList[4]),             # Wind Direction 0-360
-        'wind_speed_kt' : int(metarList[5]),                # Wind Speed Knots  11
-        'wind_gust_kt' : setWindGust(metarList[5],metarList[6]),                 # Wind Speed Knots  15
-        #vis & pressure
-        'visibility_statute_mi' : setFloat(metarList[7]),      # Visibility in Statute Miles   24.2
-        'altim_in_hg' : float(metarList[8]),                # Pressure in inches of Mercury rounded to 2 decimals    29.92
-        #report correct and maintenance indicator
-        'corrected' : setBool(metarList[9]),                        # Is report a correction   True
-        'maintenance_indicator_on' : setBool(metarList[10]),         # Is the weather station due for maintenance  False
-        #present weather values
-        #TODO: The following wxtsring fields are a very basic conversion of wxstring, if time, consider a more elegant way to do this
-        'wx_string' : presentWxString,                       # TODO: Should I convert this to something else or just leave it as a string, perhaps index it so easily searched in db?   e.g. VCTS +RA
-        'precip_rain': precipList[0],     #all four precip types return 0 if not reported. 1 = light, 2 = moderate, 3 = heavy , NOTE: VC for vicinity is assumed to be present weather e.g. 1
-        'precip_snow': precipList[1],
-        'precip_drizzle': precipList[2],
-        'precip_unknown': precipList[3],
-        'precip_intensity': max(precipList),   #max intensity of all precip types
-        'precip_occuring': True if max(precipList) > 0 else False,
-        'thunderstorm': setWxTypeBoolean(presentWxString,['TS']),                       #If TS in string, then tstorm occuring  True/False
-        'hail_graupel_pellets': setWxTypeBoolean(presentWxString,['GR','GS','PL']),                        #If any in list then true, else false
-        'fog_mist': setWxTypeBoolean(presentWxString,['BR','FG']),                            #If any in list then true, else false
-        'tornado': setTornado(remark,presentWxString),
-        #precip and pressure from remarks
-        'three_hour_pressure_tendency':setThreeHrPressureTendency(remark),
-        'one_hour_precip':setOneHrPrecip(remark),
-        #Cloud cover
-        'transmissivity_clouds' : 9.99,             # TODO: Transmissivity of Clouds based on NRCC paper, a very interesting value, but it will take a while to implement e.g. .96
-        'max_cloud_cov' : setMaxCloudCover(metarList[12:20]),                     #Max Cloud Percentage based on METAR standards, for details see function above   e.g. .1275
-        #other keys/vals
-        'metar_type' : metarList[20],               # Report SPECI or METAR     e.g. METAR/SPECI
-        'remark' : setRemark(metarList)                  # String with additional info such as SYNOP code  e.g. AO2 LTG DSNT SE AND S P0004 T01330128
+    return{"precipitation_liquid_departure" : setFloat(climateFlatDict,"precipitation_liquid_departure"),
+        "precipitation_liquid_new_record" : setBool(climateFlatDict,"precipitation_liquid_new_record"),
+        "precipitation_liquid_observed" : setFloat(climateFlatDict,"precipitation_liquid_observed"),
+        "precipitation_snowfall_departure" : setFloat(climateFlatDict,"precipitation_snowfall_departure"),
+        "precipitation_snowfall_new_record" : setBool(climateFlatDict,"precipitation_snowfall_new_record"),
+        "precipitation_snowfall_observed" : setFloat(climateFlatDict,"precipitation_snowfall_observed"),
+        "report_date" : climateFlatDict["report-date"],
+        "skies_average_sky_cover" : setFloat(climateFlatDict,"skies_average_sky_cover"),
+        "icao_id" : climateFlatDict["station"],
+        "total_sun_potential" : setSunToMins(climateFlatDict["sun_sunrise"],climateFlatDict["sun_sunset"]),
+        "temperature_maximum_departure" : setFloat(climateFlatDict,"temperature_maximum_departure"),
+        "temperature_maximum_new_record" : setBool(climateFlatDict,"temperature_maximum_new_record"),
+        "temperature_maximum_observed" : setFloat(climateFlatDict,"temperature_maximum_observed"),
+        "temperature_minimum_departure" : setFloat(climateFlatDict,"temperature_minimum_departure"),
+        "temperature_maximum_new_record" : setBool(climateFlatDict,"temperature_minimum_new_record"),
+        "temperature_minimum_observed" : setFloat(climateFlatDict,"temperature_minimum_observed"),
+        "winds_average_wind_speed" : setFloat(climateFlatDict,"winds_average_wind_speed"),
+        "winds_highest_gust_speed" : setFloat(climateFlatDict,"winds_highest_gust_speed"),
+        "winds_highest_wind_speed" : setFloat(climateFlatDict,"winds_highest_wind_speed"),
+        "report_start_datetime": datesTimes[0],
+        "report_end_datetime": datesTimes[1],
+        
+        "avg_two_day_precipitation_liquid_departure" : -999.9,
+        "avg_two_day_precipitation_liquid_observed" : -999.9,
+        "avg_two_day_precipitation_snowfall_departure" : -999.9,
+        "avg_two_day_precipitation_snowfall_observed" : -999.9,
+        "avg_two_day_temperature_maximum_departure" : -999.9,
+        "avg_two_day_temperature_maximum_observed" : -999.9,
+        "avg_two_day_temperature_minimum_departure" : -999.9,
+        "avg_two_day_temperature_minimum_observed" : -999.9,
+        "avg_two_day_winds_average_wind_speed" : -999.9,
+        
+        "avg_three_day_precipitation_liquid_departure" : -999.9,
+        "avg_three_day_precipitation_liquid_observed" : -999.9,
+        "avg_three_day_precipitation_snowfall_departure" : -999.9,
+        "avg_three_day_precipitation_snowfall_observed" : -999.9,
+        "avg_three_day_temperature_maximum_departure" : -999.9,
+        "avg_three_day_temperature_maximum_observed" : -999.9,
+        "avg_three_day_temperature_minimum_departure" : -999.9,
+        "avg_three_day_temperature_minimum_observed" : -999.9,
+        "avg_three_day_winds_average_wind_speed" : -999.9,
+        
+        "avg_seven_day_precipitation_liquid_departure" : -999.9,
+        "avg_seven_day_precipitation_liquid_observed" : -999.9,
+        "avg_seven_day_precipitation_snowfall_departure" : -999.9,
+        "avg_seven_day_precipitation_snowfall_observed" : -999.9,
+        "avg_seven_day_temperature_maximum_departure" : -999.9,
+        "avg_seven_day_temperature_maximum_observed" : -999.9,
+        "avg_seven_day_temperature_minimum_departure" : -999.9,
+        "avg_seven_day_temperature_minimum_observed" : -999.9,
+        "avg_seven_day_winds_average_wind_speed" : -999.9,
+        
+        "avg_thirty_day_precipitation_liquid_departure" : -999.9,
+        "avg_thirty_day_precipitation_liquid_observed" : -999.9,
+        "avg_thirty_day_precipitation_snowfall_departure" : -999.9,
+        "avg_thirty_day_precipitation_snowfall_observed" : -999.9,
+        "avg_thirty_day_temperature_maximum_departure" : -999.9,
+        "avg_thirty_day_temperature_maximum_observed" : -999.9,
+        "avg_thirty_day_temperature_minimum_departure" : -999.9,
+        "avg_thirty_day_temperature_minimum_observed" : -999.9,
+        "avg_thirty_day_winds_average_wind_speed" : -999.9
     }
 
+import flatdict
+from wxtalk import helper
+import os
+import os.path
 
+#get all file names and build base dictionary (each station is represented by ICAO.json and values are list of all reports       
+files = []
+stationsDict = {}  # {'KSEA.json':[{},{}]
+indir = '/home/steven/Desktop/T/WeatherTalk/wxtalk/wxcollector/tests/test-data'
+datadir = indir +  "/climate"
+for dirpath, dirnames, filenames in os.walk(datadir):
+    for filename in [f for f in filenames if f.endswith(".json")]:
+        files.append( os.path.join(dirpath, filename))
+        stationsDict[filename] = []
+
+files.sort() #sort files
+
+#load each daily report to appropriate station key list
+#put key names in correct format
+for file in files:
+    #try:
+    climDict = helper.loadJSONfromFile(file)
+    #print climDict
+    flatWxDict = flatdict.FlatDict(climDict[climDict.keys()[0]])
+    outputDict = {}
+    keysList = flatWxDict.keys()
+    for key in keysList:
+        outputDict[key.replace(':','_').replace(' ','_').lower()]=(flatWxDict[key])
+        
+    #get desired values for output dict
+    outputDict = setClimateDict(outputDict)
+    stationsDictKeyName = file.split('/')[-1]  #retrieve original file name e.g. 'KSEA.json' this is the key name
+    stationsDict[stationsDictKeyName].append(outputDict)
+
+
+#update each field to correct output format
+#output each list of dicts of each station to a json file    
+for key in stationsDict:   
+    helper.dumpJSONtoFile(indir +'/' + key,stationsDict[key])
