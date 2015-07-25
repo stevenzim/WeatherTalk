657d1f7a06ed9745df9e9b387b619d8b3ddac851
stevenzim <stevenedwardzimmerman@gmail.com>
2015-07-25 23:48:19 +0100

    Created model ensembler

--- wxtalk/modelbuilder/ensemblers.py
+++ wxtalk/modelbuilder/ensemblers.py
@@ -12,21 +12,40 @@ Key steps
     d - add key values for probability of winning class (which is value of 2b which is maximum)
 3 - return the updated list of dict
  '''
- 
-#example to update dicts in list
-#l = [{'key1':1}]
-#for i in enumerate(l):
-#    n = i[0]
-#    d = i[1]
-#    d['key2'] =3
-#    l[n] = d
- 
+
 
 from wxtalk.modelbuilder import transformers
 from wxtalk import helper
 
+import itertools
 
-                    
+def compileEnsemble(listOfEnsembleMetaDicts,listOfTweets):
+    '''Provided ensemble params and list of tweet dicts, returns tweet dicts with probabilistic results for winning class and 
+    discrete value of winning class for each ensemble, see module doc string for more details'''
+    listOfTweetDicts = listOfTweets
+    for ensemble in listOfEnsembleMetaDicts:
+        print "Predicting results for ensemble: " + ensemble["description"]
+        modelIds = ensemble["model_id_list"]
+        for dict in enumerate(listOfTweetDicts):
+            idx = dict[0]
+            tweet = dict[1]
+            allModelProbas4Tweet = []
+            try:
+                for modelId in modelIds:
+                    allModelProbas4Tweet.append(tweet[modelId + '_proba'])
+            except:
+                raise Exception("Probabilistic results for model id in ensemble model_id_list is not in tweet")
+            #sum all model probabiliies and produce mean value per Webis paper
+            sumModelProbsList = map(lambda sublist: sum(sublist), itertools.izip(*allModelProbas4Tweet))
+            meanProbs = map(lambda probSum: round(probSum/len(modelIds),3),sumModelProbsList)
+            #get winning idx, class and probability
+            winningIndex = meanProbs.index(max(meanProbs))
+            winningClass = ensemble["model_classes"][winningIndex]
+            winningProba = meanProbs[winningIndex]
+            tweet[ensemble["id"] + "_discrete"] = winningClass
+            tweet[ensemble["id"] + "_proba"] = winningProba
+            listOfTweetDicts[idx] = tweet
+        
+    return listOfTweetDicts
 
-def compileEnsemble(listOfEnsembleMetaDicts,listOfTweetDicts):
-    return None
+
--- wxtalk/modelbuilder/tests/test__ensemblers.py
+++ wxtalk/modelbuilder/tests/test__ensemblers.py
@@ -5,41 +5,48 @@ from wxtalk import helper
 
 testTweetWithPreds1 = [{"text":"Bad day :(",\
                 "id":"test1",\
-                "model_preds":{
-                    "s1_discrete":-1,\
-                    "s1_proba":[.2,.7,.1],\
-                    "s2_discrete":-1,\
-                    "s2_proba":[.8,.1,.1]}}]
-
+                "s1_discrete":-1,\
+                "s1_proba":[.2,.7,.1],\
+                "s2_discrete":-1,\
+                "s2_proba":[.8,.1,.1]}]
+testTweetWithPreds2 = [{"text":"Bad day :(",\
+                "id":"test1",\
+                "s1_discrete":-1,\
+                "s1_proba":[.2,.7,.1],\
+                "s2_discrete":-1,\
+                "s2_proba":[.8,.1,.1]}]
+testTweetWithPreds3 = [{"text":"Bad day :(",\
+                "id":"test1",\
+                "s1_discrete":-1,\
+                "s1_proba":[.2,.7,.1],\
+                "s2_discrete":-1,\
+                "s2_proba":[.8,.1,.1]}]
 expectedTweetWithPreds1 = [{"text":"Bad day :(",\
                             "id":"test1",\
-                            "model_preds":{
-                                "s1_discrete":-1,\
-                                "s1_proba":[.2,.7,.1],\
-                                "s2_discrete":-1,\
-                                "s2_proba":[.8,.1,.1]},\
+                            "s1_discrete":-1,\
+                            "s1_proba":[.2,.7,.1],\
+                            "s2_discrete":-1,\
+                            "s2_proba":[.8,.1,.1],\
                             'ens_s1_discrete':-1,\
-                            'ens_s1_prob':.5}]
+                            'ens_s1_proba':.5}]
 expectedTweetWithPreds2 = [{"text":"Bad day :(",\
                             "id":"test1",\
-                            "model_preds":{
-                                "s1_discrete":-1,\
-                                "s1_proba":[.2,.7,.1],\
-                                "s2_discrete":-1,\
-                                "s2_proba":[.8,.1,.1]},\
+                            "s1_discrete":-1,\
+                            "s1_proba":[.2,.7,.1],\
+                            "s2_discrete":-1,\
+                            "s2_proba":[.8,.1,.1],\
                             'ens_s2_discrete':0,\
-                            'ens_s2_prob':.5}]
+                            'ens_s2_proba':.5}]
 expectedTweetWithPreds3 = [{"text":"Bad day :(",\
                             "id":"test1",\
-                            "model_preds":{
-                                "s1_discrete":-1,\
-                                "s1_proba":[.2,.7,.1],\
-                                "s2_discrete":-1,\
-                                "s2_proba":[.8,.1,.1]},\
+                            "s1_discrete":-1,\
+                            "s1_proba":[.2,.7,.1],\
+                            "s2_discrete":-1,\
+                            "s2_proba":[.8,.1,.1],\
                             'ens_s1_discrete':-1,\
-                            'ens_s1_prob':.5,\
+                            'ens_s1_proba':.5,\
                             'ens_s2_discrete':0,\
-                            'ens_s2_prob':.5}]
+                            'ens_s2_proba':.5}]
 ensemble1params = {"id":'ens_s1',\
             "model_id_list":['s1','s2'],\
             "model_classes":[-1,0,1],\
@@ -53,15 +60,17 @@ ensemble2params = {"id":'ens_s2',\
 #TODO: Test for mismatch of len(model_classes) and len(model_proba)
 #TODO: Test for mismatch/unavailability of model id
 
-def test_ensembles():
-    '''Test to confirm discrete class and probabilitic weighted average of that class is returned'''
-    #test 1 - equalweights
-    results = ensemblers.compileEnsemble([ensemble1params],testTweetWithPreds1)
-    assert_equal(results,expectedTweetWithPreds1)
+def test_one_ensemble():
+    '''Test to confirm  discrete class and probabilitic weighted average of that class is returned'''
+#    #test 1 - equalweights
+    results1 = ensemblers.compileEnsemble([ensemble1params],testTweetWithPreds1)
+    assert_equal(results1,expectedTweetWithPreds1)
     #test 2 - non equalweights
-    results = ensemblers.compileEnsemble([ensemble1params,ensemble2params],testTweetWithPreds1)
-    assert_equal(results,expectedTweetWithPreds2)
-    #test 3 - combined results
-    results = ensemblers.compileEnsemble([ensemble1params,ensemble2params],testTweetWithPreds1)
-    assert_equal(results,expectedTweetWithPreds3)
+    results2 = ensemblers.compileEnsemble([ensemble2params],testTweetWithPreds2)
+    assert_equal(results2,expectedTweetWithPreds2)
+def test_two_ensembles():
+    '''Test to confirm multiple ensembles added for discrete class and probabilitic weighted average of that class is returned'''
+#    #test 3 - combined results
+    results3 = ensemblers.compileEnsemble([ensemble1params,ensemble2params],testTweetWithPreds1)
+    assert_equal(results3,expectedTweetWithPreds3)
 
