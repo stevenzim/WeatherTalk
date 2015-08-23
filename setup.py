4b6b8c6e968910336c29c71e05fb45a2de3faf6d
stevenzim <stevenedwardzimmerman@gmail.com>
2015-08-23 09:25:34 +0100

    Model builder files

--- setup.py
+++ setup.py
@@ -5,15 +5,14 @@ except ImportError:
 
 config = {
     'description': 'MSc Thesis Code',
-    'author': 'Steven Zimmerman',
+    'author': 'Steven E. Zimmerman',
     'url': 'https://github.com/stevenzim/WeatherTalk',
     'download_url': 'https://github.com/stevenzim/WeatherTalk',
     'author_email': 'szimme@essex.ac.uk',
-    'version': '0.1',
-    'install_requires': [],
-    'packages': ['tests','tweetcollector','wxcollector','db','models'],
-    'scripts': [],
-    'name': 'wxtalk'
+    'version': '1.0',
+    'install_requires': ['TwitterAPI','sklearn','numpy','nltk','psycopg2'],
+    'packages': ['tests','tweetcollector','wxcollector','db','model'],
+    'name': 'WeatherTalk'
 }
 
 setup(**config)
